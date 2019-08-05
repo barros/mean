8fc551f7d37a938f0ff3a5af619a2a8b6aae3587
barros <barrospj@bc.edu>
2019-08-04 23:56:07 -0400

    configure MongoDB

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
+const config = require('./config/database')
+
+// Connect to database
+mongoose.connect(config.database);
+// On connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to db: '+config.database)
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log('*** MongoDB error: '+err)
+})
 
 const app = express();
-const users = require('./routes/users');
+// Port number
+const port = 3000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/users', users);
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-const port = 3000;
+// Routes
+const users = require('./routes/users');
+app.use('/users', users);
 
-// start server
+// Start server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
 });
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: "mongodb://localhost:27017/meansample",
+  secret: process.env.MONGODB_SECRET
+}
