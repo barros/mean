65da543fe06d03e1864d9b05f7954b29ead8e121
barros <barrospj@bc.edu>
2019-08-05 22:27:12 -0400

    update /user/register POST endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,38 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
-router.get('/register', (req, res, next) => {
-  res.send('REGISTER')
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    // Name not required at registration
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  // Add user to db
+  User.addUser(newUser, (err, user) => {
+    if (err){
+      res.json({success: false, msg: "Failed to register user"});
+    } else {
+      res.json({success: true, msg: "User registered"});
+    }
+  })
 });
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-  res.send('AUTHENTICATE')
+  res.send('AUTHENTICATE');
 });
 
 // Profile
 router.get('/profile', (req, res, next) => {
-  res.send('PROFILE')
-});
-
-// Validate
-router.get('/validate', (req, res, next) => {
-  res.send('VALIDATE')
+  res.send('PROFILE');
 });
 
 module.exports = router;
